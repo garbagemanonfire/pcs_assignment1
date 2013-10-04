5f50392a83b0dbc4efad5ba7ceb8abc106a554fc
garbagemanonfire <nilesvm@gmail.com>
2013-10-04 14:10:13 -0700

    Commented out lines at bottom correctly.

--- variables.js
+++ variables.js
@@ -1,19 +1,19 @@
-var productName
-var currentPrice
-var totalCost
-var productTax
+var productName;
+var currentPrice;
+var totalCost;
+var productTax;
 
-productName = "cookies"
-currentPrice = 3
-productTax = currentPrice * .07
-totalCost = currentPrice + productTax
-console.log("Your " + productName + " cost $" + totalCost)
+productName = "cookies";
+currentPrice = 3;
+productTax = currentPrice * .07;
+totalCost = currentPrice + productTax;
+console.log("Your " + productName + " cost $" + totalCost);
 
-# create a javascript file (variables.js) that 
-# creates some variables 
-# at least 1 string, and a number,
-# assigns some values, 
-# creates a complex expression, 
-# runs an operation, 
-# give output via console.log (i.e. console.log FullName), 
-# put it into PCS_Assignment_1 repo
+// create a javascript file (variables.js) that 
+// creates some variables 
+// at least 1 string, and a number,
+// assigns some values, 
+// creates a complex expression, 
+// runs an operation, 
+// give output via console.log (i.e. console.log FullName), 
+// put it into PCS_Assignment_1 repo
